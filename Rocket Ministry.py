553f2d8c136e65cf11da330165e30d16ab5a163e
Anatol <antorix@gmail.com>
2022-10-19 23:10:42 +0300

    Update Rocket Ministry.py

--- Rocket Ministry.py
+++ Rocket Ministry.py
@@ -3,16 +3,16 @@
 import urllib.request
 from os import path
 
-url = "https://github.com/antorix/Rocket-Ministry/releases/download/Python/rocket_ministry_install_script.py"
+installFile = "rm_install.py"
 
-installFile = "rm-install.py"
+url = "https://github.com/antorix/Rocket-Ministry/releases/download/Python/" + installFile
 
 if not path.exists("main.py"):
 
     if not path.exists(installFile):
         urllib.request.urlretrieve(url, installFile)
 
-    from rm-install import install
+    from rm_install import install
     install(desktop=False)
 
 from main import app
